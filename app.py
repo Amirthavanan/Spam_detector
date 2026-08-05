44d2a70bd224ef1f049862705acfb56a2c92ad74
Amirthavanan <100839355+Amirthavanan@users.noreply.github.com>
2026-08-05 17:16:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ for pkg, path in [
 
 stop_words = set(stopwords.words("english"))
 
-MODEL_PATH="/content/drive/MyDrive/wrenchwise/logistic_regression_model.joblib"
-VEC_PATH="/content/drive/MyDrive/wrenchwise/tfidf_vectorizer.joblib"
+MODEL_PATH="logistic_regression_model.joblib"
+VEC_PATH="tfidf_vectorizer.joblib"
 
 @st.cache_resource
 def load():
