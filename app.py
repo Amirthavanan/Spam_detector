e287fb0ea2ef9b5a980ecfe0b5fec465fa2f40f9
Amirthavanan <100839355+Amirthavanan@users.noreply.github.com>
2026-08-07 11:48:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ st.markdown("""
 
 c1,c2,c3 = st.columns(3)
 with c1:
-    st.markdown('<div class="metric"><h3>98%+</h3><small>Accuracy</small></div>', unsafe_allow_html=True)
+    st.markdown('<div class="metric"><h3>98%</h3><small>Accuracy</small></div>', unsafe_allow_html=True)
 with c2:
     st.markdown('<div class="metric"><h3>TF-IDF</h3><small>Vectorizer</small></div>', unsafe_allow_html=True)
 with c3:
